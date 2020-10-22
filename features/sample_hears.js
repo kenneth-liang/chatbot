8d1978311325dd9f360c66996084ca7dc72d67b5
Kenneth <kennethkjliang@gmail.com>
2020-10-21 22:05:47 -0700

    test hears

--- features/sample_hears.js
+++ features/sample_hears.js
@@ -1,8 +1,60 @@
+const { BotkitConversation } = require("botkit")
+
 /**
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
+ 
+
+// const phrase = "What would you like to learn about Kenneth?"
+// const joke = "How about a joke to cheer you up? (yes/no)"
+
+
 module.exports = function(controller) {
+
+
+  
+  // controller.hears(
+  //   new RegExp(/(hi|hello|howdy|hey)/i),
+  //   ["message", "direct_message"],
+  //   async function (bot, message) {
+  //     // await bot.beginDialog('typing');
+  //     await bot.reply(message, { text: "Hi! My name is Kenneth. How are you today?" });
+  //   }
+  // );
+
+
+
+  // controller.hears(
+  //   [new RegExp(/bad/i),new RegExp(/(ok|okay)/i), new RegExp(/(soso|so-so)/i)],
+  //   ["message", "direct_message"],
+  //   async function (bot, message) {
+  //     await bot.beginDialog('typing');
+  //     await bot.reply(message, {
+  //       text:
+  //         `Oh, sorry to hear that. ${joke}`,
+  //     });
+  //   }
+  // );
+
+  // controller.hears(
+  //   new RegExp(/(yes|y|joke)/i),
+  //   ["message", "direct_message"],
+  //   async function (bot, message) {
+  //     await bot.beginDialog('typing');
+  //     await bot.reply(message, {
+  //       text:
+  //         `How do robots eat guacamole?`,
+  //     });
+  //     await bot.beginDialog('typing');
+  //     await bot.reply(message, {
+  //       text:
+  //         `With Microchips!`,
+  //     });
+  //   }
+  // );
+
+
 
     // use a function to match a condition in the message
     controller.hears(async (message) => message.text && message.text.toLowerCase() === 'foo', ['message'], async (bot, message) => {
