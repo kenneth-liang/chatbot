b5fe79947fb2ee224affbf2a1cc6cf2e57903fe2
Kenneth <kennethkjliang@gmail.com>
2020-10-21 22:06:51 -0700

    edited typing

--- features/typing.js
+++ features/typing.js
@@ -8,7 +8,7 @@ module.exports = function(controller) {
 
     let typing = new BotkitConversation('typing', controller);
 
-    typing.say('I am going to type for a while now...');
+    // typing.say('I am going to type for a while now...');
     typing.addAction('typing');
 
     // start the typing indicator
@@ -16,7 +16,7 @@ module.exports = function(controller) {
     // trigger a gotoThread, which gives us an opportunity to delay the next message
     typing.addAction('next_thread','typing');
 
-    typing.addMessage('typed!','next_thread');
+    typing.addMessage({type: 'typing'},'next_thread');
 
     // use the before handler to delay the next message 
     typing.before('next_thread',  async () => {
@@ -32,13 +32,12 @@ module.exports = function(controller) {
         await bot.beginDialog('typing');
     });
 
-    controller.hears('typing reply', 'message', async (bot, message) => {
+    controller.on('message', async (bot, message) => {
 
         await bot.reply(message, {type: 'typing'});
         setTimeout(async () => {
             // will have to reset context because turn has now ended.
             await bot.changeContext(message.reference);
-            await bot.reply(message, 'Typed!');
         }, 1000);
     });
 };
